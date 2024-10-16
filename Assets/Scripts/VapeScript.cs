f6ac3a994be62ff5225edcacf6eaa4536ed50df4
Zilvinas-Jonaitis-TUS22 <k00281776@student.tus.ie>
2024-10-16 09:56:25 +0100

    Vape working

--- Assets/Scripts/VapeScript.cs
+++ Assets/Scripts/VapeScript.cs
@@ -1,18 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+using StarterAssets;
 
 public class VapeScript : MonoBehaviour
 {
+    public Slider vapeJuiceSlider; // Reference to the slider UI component
+    public Animator animator; // Reference to the Animator component
+    public float vapeJuiceAmount = 100f; // Total amount of vape juice
+    public float consumptionRate = 1f; // Amount of vape juice consumed per second
+
+    private StarterAssetsInputs _Inputs;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        vapeJuiceSlider.maxValue = vapeJuiceAmount; // Set the max value of the slider
+        vapeJuiceSlider.value = vapeJuiceAmount; // Initialize the slider to full vape juice
+
+        _Inputs = GetComponent<StarterAssetsInputs>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (_Inputs.vaping)
+        {
+            Debug.Log("Vaping");
+            // Reduce the vape juice over time
+            vapeJuiceSlider.value -= consumptionRate * Time.deltaTime;
+
+            // Check if the vape juice is empty
+            if (vapeJuiceSlider.value <= 0)
+            {
+                vapeJuiceSlider.value = 0;
+                StopVaping(); // Stop vaping if the juice is finished
+            }
+            else
+            {
+                // Set the "Currently Vaping" parameter to true while vaping
+                animator.SetBool("Currently Vaping", true);
+            }
+        }
+        else
+        {
+            Debug.Log("Not Vaping");
+            // Set the "Currently Vaping" parameter to false when not vaping
+            animator.SetBool("Currently Vaping", false);
+        }
+    }
+
+    public void StartVaping()
+    {
+        _Inputs.vaping = true;
+        // The animator will set "Currently Vaping" to true in Update
+    }
+
+    public void StopVaping()
+    {
+        _Inputs.vaping = false;
+        // The animator will set "Currently Vaping" to false in Update
     }
 }
--- Assets/StarterAssets/InputSystem/StarterAssetsInputs.cs
+++ Assets/StarterAssets/InputSystem/StarterAssetsInputs.cs
@@ -12,8 +12,9 @@ namespace StarterAssets
 		public Vector2 look;
 		public bool jump;
 		public bool sprint;
+        public bool vaping;
 
-		[Header("Movement Settings")]
+        [Header("Movement Settings")]
 		public bool analogMovement;
 
 		[Header("Mouse Cursor Settings")]
@@ -43,10 +44,15 @@ namespace StarterAssets
 		{
 			SprintInput(value.isPressed);
 		}
+
+        public void OnVaping(InputValue value)
+        {
+            VapeInput(value.isPressed);
+        }
 #endif
 
 
-		public void MoveInput(Vector2 newMoveDirection)
+        public void MoveInput(Vector2 newMoveDirection)
 		{
 			move = newMoveDirection;
 		} 
@@ -65,8 +71,13 @@ namespace StarterAssets
 		{
 			sprint = newSprintState;
 		}
-		
-		private void OnApplicationFocus(bool hasFocus)
+
+        public void VapeInput(bool vapeState)
+        {
+            vaping = vapeState;
+        }
+
+        private void OnApplicationFocus(bool hasFocus)
 		{
 			SetCursorState(cursorLocked);
 		}
