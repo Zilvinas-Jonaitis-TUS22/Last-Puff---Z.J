dbc97a28e0fe1a00fa1293f60c071d5f7d930424
Zilvinas-Jonaitis-TUS22 <k00281776@student.tus.ie>
2024-11-05 09:51:18 +0000

    sorted files + added sprint bar

---
+++ Assets/Scripts/Vape Related Script/CollectableVape.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableVape : MonoBehaviour
+{
+    public float refillAmount = 20f; // Amount to refill the vape bar
+    public string vapeID; // Unique ID for this vape object
+
+    private VapeScript vapeScript;
+    public AudioSource collectSound;
+
+    private bool isCollected = false; // Track collection status
+
+    void Start()
+    {
+        vapeScript = FindObjectOfType<VapeScript>();
+
+        if (collectSound == null)
+        {
+            collectSound = gameObject.AddComponent<AudioSource>();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !isCollected)
+        {
+            isCollected = true; // Mark as collected
+
+            // Refills the vape bar
+            if (vapeScript != null)
+            {
+                vapeScript.vapeJuiceSlider.value += refillAmount;
+                vapeScript.vapeJuiceSlider.value = Mathf.Clamp(vapeScript.vapeJuiceSlider.value, 0, vapeScript.vapeJuiceAmount);
+            }
+
+            // Play the collection sound
+            if (collectSound != null && collectSound.clip != null)
+            {
+                collectSound.Play();
+            }
+
+            // Disable the object after collecting
+            gameObject.SetActive(false);
+        }
+    }
+}
